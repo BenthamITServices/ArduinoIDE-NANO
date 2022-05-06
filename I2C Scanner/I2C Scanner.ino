30bf2bb5ee43fcd5387c2290b153646121741482
Steven Bennett <2668285+BenthamITServices@users.noreply.github.com>
2022-05-06 12:29:19 +0100

    Description of operation added
    Loops from address 1 to 127 and if no I2C transmission error the address is sent to serial.

---
+++ I2C Scanner/I2C Scanner.ino
@@ -0,0 +1,45 @@
+/********* x
+  Rui Santos
+  Complete project details at https://randomnerdtutorials.com  
+*********/
+
+// Loops from address 1 to 127 and if no I2C transmission error the address is sent to serial
+
+#include <Wire.h>
+
+void setup() {
+  Wire.begin();
+  Serial.begin(9600);
+  Serial.println("\nI2C Scanner");
+}
+
+void loop() {
+  byte error, address;
+  int nDevices;
+  Serial.println("Scanning...");
+  nDevices = 0;
+  for (address = 1; address < 127; address++) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address < 16) {
+        Serial.print("0");
+      }
+      Serial.println(address, HEX);
+      nDevices++;
+    } else if (error == 4) {
+      Serial.print("Unknow error at address 0x");
+      if (address < 16) {
+        Serial.print("0");
+      }
+      Serial.println(address, HEX);
+    }
+  }
+  if (nDevices == 0) {
+    Serial.println("No I2C devices found\n");
+  } else {
+    Serial.println("done\n");
+  }
+  delay(1000);
+}
