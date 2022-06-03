8d311f26e25877405177bc5133c37e6d017be409
Steven Bennett <2668285+BenthamITServices@users.noreply.github.com>
2022-06-03 13:13:24 +0000

    Code formatted
    Using Auto Format in Arduino IDE 2

--- Modify_PWM_frequency/Modify_PWM_frequency.ino
+++ Modify_PWM_frequency/Modify_PWM_frequency.ino
@@ -11,26 +11,26 @@ void setup() {
   TCCR2B = TCCR2B | 0b00001011;  // sets lower 4 bits of TCCR2B to 1011
   OCR2A = 0xF8;                  // OCR2A sets the top value for the PWM counter. E8 = 1.08KHz, F8 = 1.01KHz, FF = 977Hz
 
-  Serial.print("\x1b[1;31m"); // Set terminal style to bold, red foreground.  See https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
+  Serial.print("\x1b[1;31m");  // Set terminal style to bold, red foreground.  See https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
 
-  Serial.print("TCCR2A = "); // Print text
-  Serial.println(TCCR2A, BIN); // Print value of TCCR2A register in binary
+  Serial.print("TCCR2A = ");    // Print text
+  Serial.println(TCCR2A, BIN);  // Print value of TCCR2A register in binary
 
-  Serial.print("TCCR2B = "); // Print text
-  Serial.println(TCCR2B, BIN); // Print value of TCCR2B register in binary
+  Serial.print("TCCR2B = ");    // Print text
+  Serial.println(TCCR2B, BIN);  // Print value of TCCR2B register in binary
 
-  Serial.print("OCR2A = "); // Print text
-  Serial.println(OCR2A, HEX); // Print value of OCR2A register in hex
-  Serial.println("========="); // Print text
+  Serial.print("OCR2A = ");     // Print text
+  Serial.println(OCR2A, HEX);   // Print value of OCR2A register in hex
+  Serial.println("=========");  // Print text
 
   delay(1000);
-  Serial.print("\x1B[1;1H\x1B[2J"); // ANSI VT100 Escape sequences to move terminal cursor to position 1,1 then clear the terminal window
+  Serial.print("\x1B[1;1H\x1B[2J");  // ANSI VT100 Escape sequences to move terminal cursor to position 1,1 then clear the terminal window
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  pot = analogRead(potPin); // read the analog voltage on potPin and convert to a value between 0 and 1023
-  mapped = map(pot, 0, 1023, 5, 250); // map a 10-bit number to an 8-bit number as a PWM analogWrite only uses 8-bits.  See: https://www.arduino.cc/reference/en/language/functions/math/map/
-  analogWrite(PWMpin, mapped); // write the value of mapped to digital pin 3 to set PWM
+  pot = analogRead(potPin);            // read the analog voltage on potPin and convert to a value between 0 and 1023
+  mapped = map(pot, 0, 1023, 5, 250);  // map a 10-bit number to an 8-bit number as a PWM analogWrite only uses 8-bits.  See: https://www.arduino.cc/reference/en/language/functions/math/map/
+  analogWrite(PWMpin, mapped);         // write the value of mapped to digital pin 3 to set PWM
   delay(100);
 }
